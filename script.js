d2f39adcda9868385757ec423d8d1a7b0b9f66ed
Gabriel Mota <107560699+gMota07@users.noreply.github.com>
2024-07-11 08:06:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+window.onload = function () {
+  slideMin();
+  slideMax();
+};
+
+const minVal = document.querySelector(".min-val");
+const maxVal = document.querySelector(".max-val");
+const priceInputMin = document.querySelector(".min-input");
+const priceInputMax = document.querySelector(".max-input");
+const minGap = 0;
+const range = document.querySelector(".slider-track");
+const sliderMinValue = parseInt(minVal.min);
+const sliderMaxValue = parseInt(maxVal.max);
+
+function slideMin() {
+  let gap = parseInt(maxVal.value) - parseInt(minVal.value);
+  if (gap <= minGap) {
+    minVal.value = parseInt(maxVal.value) - minGap;
+  }
+  priceInputMin.value = minVal.value;
+  setArea()
+}
+
+function slideMax() {
+  let gap = parseInt(maxVal.value) - parseInt(minVal.value);
+  if (gap <= minGap) {
+    maxVal.value = parseInt(minVal.value) + minGap;
+  }
+  priceInputMax.value = maxVal.value;
+  setArea()
+}
+
+function setArea() {
+  range.style.left = (minVal.value / sliderMaxValue) * 100 + "%";
+  range.style.right = 100 - (maxVal.value / sliderMaxValue) * 100 + "%";
+}
+
+
+
